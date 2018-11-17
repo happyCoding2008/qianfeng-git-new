6ab61d9029ba293431135919852e64c3f0016454
happycodingnow <happycodingnow@sina.com>
2018-11-17 14:07:32 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/qianfenggitnew/QianfengGitNewApplication.java
@@ -0,0 +1,12 @@
+package com.qianfeng.qianfenggitnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianfengGitNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QianfengGitNewApplication.class, args);
+	}
+}
---
+++ src/main/java/com/qianfeng/qianfenggitnew/controller/GitController.java
@@ -0,0 +1,19 @@
+package com.qianfeng.qianfenggitnew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author HuangGuiZhao
+ * @Date 2018/11/17
+ */
+@Controller
+@RequestMapping("git")
+public class GitController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        System.out.println("happyCoding2008");
+        return "hello";
+    }
+}
